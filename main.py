bd33251cf5b9eef0c285f2bbd67c58bab942a8b9
ShuntaYamazaki <yamazaki.s.ap@m.titech.ac.jp>
2025-06-14 14:37:46 +0900

    langchainを使ってツールを呼び出すコード作成した

--- main.py
+++ main.py
@@ -1,6 +1,44 @@
-def main() -> None:
-    print("Hello from langchain-agent!")
+from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
+from langchain_core.tools import tool
+from pydantic import BaseModel, Field
+
+load_dotenv()
 
 
-if __name__ == "__main__":
-    main()
+@tool
+def add(a: int, b: int) -> int:
+    """Adds a and b."""
+    return a + b
+
+
+@tool
+def multiply(a: int, b: int) -> int:
+    """Multiplies a and b."""
+    return a * b
+
+
+class Add(BaseModel):
+    """Add two integers together."""
+
+    a: int = Field(..., description="First integer")
+    b: int = Field(..., description="Second integer")
+
+
+class Multiply(BaseModel):
+    """Multiply two integers together."""
+
+    a: int = Field(..., description="First integer")
+    b: int = Field(..., description="Second integer")
+
+
+tools = [add, multiply]
+
+llm = init_chat_model("gpt-4o-mini", model_provider="openai")
+
+llm_with_tools = llm.bind_tools(tools)
+
+query = "What is 3 * 12?"
+
+result = llm_with_tools.invoke(query)
+print(result)
